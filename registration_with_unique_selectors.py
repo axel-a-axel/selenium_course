f4341361cd13317082946b9e6ca1eee93c553d93
korzia <Qwer2345>
2022-08-18 12:27:49 +0300

    some more tasks done

---
+++ registration_with_unique_selectors.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try:
+    link = "https://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    first_name = browser.find_element(By.XPATH, '//input[@class="form-control first" and @required]')
+    first_name.send_keys('a')
+
+    last_name = browser.find_element(By.XPATH, '//input[@class="form-control second" and @required]')
+    last_name.send_keys('a')
+
+    email = browser.find_element(By.XPATH, '//input[@class="form-control third" and @required]')
+    email.send_keys('a')
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(3)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
